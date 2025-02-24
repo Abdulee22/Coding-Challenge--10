1877a7f0268c2b6d418f8b454e2b3106ed03dc88
Abdulhakim <Abdulthe383@gmail.com>
2025-02-23 22:28:57 -0500

    Task 2: Creating an Order Class

--- cc_10.js
+++ cc_10.js
@@ -15,3 +15,17 @@ const prod1 = new Product("Laptop", 101, 1200, 10); // New product
 console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 10"
 prod1.updateStock(3); // Reduce stock by 3 units
 console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+// Task 2: Creating an Order Class //
+class Order {
+constructor(orderId, product, quantity) {
+this.orderId = orderId; // Order ID
+this.product = product; // Product 
+this.quantity = quantity; // Quantity 
+this.totalPrice = product.price * quantity; // Total price 
+this.product.updateStock(quantity);} // Lowers stock 
+getOrderDetails() {
+return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}`;}}
+const order1 = new Order(501, prod1, 2); // New order
+console.log(order1.getOrderDetails()); //Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
